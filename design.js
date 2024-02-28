4c6d73b0f8f686689cf650bf2f5d1fa3a20f3262
Nahid Hasan <146347290+alimnh520@users.noreply.github.com>
2024-02-28 22:00:54 +0600

    main

--- design.js
+++ design.js
@@ -1,18 +1,60 @@
-let slideBar = document.querySelector(".slider");
-slideBar.classList.add("hidden");
+let getBody = document.querySelector("body");
+let getBox = document.querySelector(".slider");
+let xMark = document.querySelector(".material-symbols-outlined");
+let menuBar = document.querySelector(".fa-bars");
 
-let showSlide = document.querySelector(".fa-bars");
-let show = true;
-showSlide.onclick = () => {
-    if (show) {
-        slideBar.classList.remove("hidden");
-        slideBar.classList.add("animate");
-    }
+// show slide menubar
+
+menuBar.addEventListener("click", () => {
+    timeOutMark();
+    getBox.classList.add("show-animate");
+    getBox.classList.remove("hide-animate");
+    ShowTimeOut();
+    bodyTime();
+});
+// x mar display
+let showMark = () => {
+    xMark.style.display = "block";
 }
-let hideSlide = document.querySelector(".fa-xmark");
-let hide = true;
-hideSlide.onclick = () => {
-    if (hide) {
-        slideBar.classList.add("hidden");
-    }
+let timeOutMark = () => {
+    setTimeout(showMark,300);
+}
+// show slide animation
+let ShowTimeOut = () => {
+    setTimeout(showSlide,300);
+}
+let showSlide = () => {
+    getBox.style.display = "block";
+}
+// body background color time
+let bodyTime = () => {
+    setTimeout(bodyOpacity,300);
+}
+let bodyOpacity = () => {
+    getBody.setAttribute("class","hide-body");
+    getBody.classList.add("hide-body::after");
+}
+
+// hide slide x bar
+
+xMark.addEventListener("click", () => {
+    xMark.style.display = "none";
+    hideTimeOut();
+    hideBodyTime();
+    getBox.classList.add("hide-animate");
+    getBox.classList.remove("show-animate");
+});
+// hide slide
+let hideTimeOut = () => {
+    setTimeout(hideSlide,200);
+}
+let hideSlide = () => {
+    getBox.style.display = "none";
+}
+// hide body background color
+let hideBodyTime = () => {
+    setTimeout(hideBody,200);
+}
+let hideBody = () => {
+    getBody.removeAttribute("class");
 }
